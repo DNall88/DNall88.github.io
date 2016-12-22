3afaa44a4bb010c158bd27ebe10ca3834ec05912
DNall88 <DNall88@gmail.com>
2016-12-22 16:06:22 +0900

    Add files via upload

---
+++ js/example.js
@@ -0,0 +1,13 @@
+function checkUserName() {
+	var elMsg = document.getElementById('feedback');
+	if (this.value.length < 5) {
+		elMsg.textContent = 'Username must be 5 characters or more';
+	}
+	else {
+		elMsg.textContent = '';
+	}
+
+}
+
+	var elUserName = document.getElementById('userEmail');
+	elUserName.onblur = checkUserName;
